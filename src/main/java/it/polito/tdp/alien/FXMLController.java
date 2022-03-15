5ccb72b68419680dbfefc1d4ab7c271c94c5376b
luca <luca@172.22.173.26>
2022-03-15 15:19:11 +0100

    Manca controllo sull'imput (poi il punto1 è concluso)

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,23 +1,69 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.alien.model.Dizionario;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+	Dizionario dizionario=new Dizionario();
+	
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private Label labelCom;
+
+    @FXML
+    private TextField txtTraduzione;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTraduzione(ActionEvent event) {
+    	String stringa =txtTraduzione.getText();
+    	txtTraduzione.clear();
+    	
+    	String[] array=stringa.split(" ");
+    	
+    	if(array.length==1) {
+    		String aliena=array[0];
+    		String traduzione=dizionario.traduzione(aliena);
+    		if(traduzione.compareTo("0")==0) {
+    			txtResult.setText("Non conosciamo ancora la traduzione di questa parola."+"\n");
+    		} else {
+    			txtResult.setText("La traduzione di "+aliena+" e': "+traduzione+".\n");
+    		}
+   
+    	}	
+    	
+    	if(array.length==2) {
+    		String aliena=array[0];
+    		String traduzione=array[1];
+    		dizionario.addTraduzione(aliena, traduzione);
+    	}
+    }
+
+    @FXML
     void initialize() {
+        assert labelCom != null : "fx:id=\"labelCom\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTraduzione != null : "fx:id=\"txtTraduzione\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtrResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
